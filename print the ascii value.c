f3705f9a412e9629ced56a83b9f185dd8c917912
Mht4r <mohit369roy@gmail.com>
2024-09-19 09:29:32 +0530

    Update and rename main.c to print the ascii value.c

---
+++ print the ascii value.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+int main()
+{
+	// print the ascii value
+	char ch;
+	printf("enter the character ");
+	scanf("%c",&ch);
+	printf("ASCII value of is %d",ch);
+
+	return 0;
+}
