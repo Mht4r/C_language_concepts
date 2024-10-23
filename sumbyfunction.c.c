15e848e6e75b955d9b59a2a93b03d341be0b0170
Mht4r <mohit369roy@gmail.com>
2024-10-23 20:22:04 +0530

    Add files via upload

---
+++ sumbyfunction.c.c
@@ -0,0 +1,18 @@
+ 
+#include <stdio.h>
+
+int sum(int x, int y){
+    
+    return x+y;
+}
+
+int main()
+{
+   //sum using function
+   
+   int c = sum(1,2); 
+   
+    printf("the sum is %d",c);
+    
+    return 0;
+}
